31ca21757d557597a0646489146f7ac2e9f8f3e3
tomp <perestom@gmail.com>
2015-10-22 15:53:32 +0300

    demo page setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -484,7 +484,7 @@ module.exports = function (grunt) {
     'cdnify',
     'cssmin',
     'uglify',
-    'filerev',
+    //'filerev',
     'usemin',
     'htmlmin'
   ]);
---
+++ demo/scripts/demo.mdl.js
@@ -0,0 +1,7 @@
+'use strict';
+angular.module('demoApp', [
+    // TODO - use my-gallery module
+]).controller('demoCtrl', function(){
+  var vm = this;
+  vm.title = 'demo';
+});
