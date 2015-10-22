eb038b9ac29e50c44ce301b045247fdd2645f481
tomp <perestom@gmail.com>
2015-10-22 13:42:33 +0300

    initial commit - bootstrapped and ready to use my-gallery module

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngIrnSrcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngIrnSrcApp
+ */
+angular.module('ngIrnSrcApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
